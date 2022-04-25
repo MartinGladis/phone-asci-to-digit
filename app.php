aa2942fe90a2ecb71ca553eebed9353a3d13600b
Martin Gladis <martin.gladis@outlook.com>
2022-04-25 23:08:12 +0200

    init program

---
+++ app.php
@@ -0,0 +1,9 @@
+<?php
+
+use App\NumericKeyboardEncoder;
+
+require __DIR__.'/vendor/autoload.php';
+
+$encoder = new NumericKeyboardEncoder();
+
+echo "Succes";
---
+++ src/NumericKeyboardEncoder.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+class NumericKeyboardEncoder
+{
+    public function __construct()
+    {
+        
+    }
+}
